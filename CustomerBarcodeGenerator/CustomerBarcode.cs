1c75fe2c3fadb9bda00e539ae107b98486f252cd
ShTair <git@vsh.jp>
2022-02-21 09:54:32 +0900

    net6に変更

--- CustomerBarcodeGenerator/CustomerBarcode.cs
+++ CustomerBarcodeGenerator/CustomerBarcode.cs
@@ -1,205 +1,201 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿namespace CustomerBarcodeGenerator;
 
-namespace CustomerBarcodeGenerator
+internal class CustomerBarcode
 {
-    class CustomerBarcode
+    public IEnumerable<Bar> Generate(string data)
     {
-        public IEnumerable<Bar> Generate(string data)
+        return GenerateBarcodeCharacters(data).SelectMany(t => ConvertToBars(t));
+    }
+
+    private static IEnumerable<BarcodeCharacter> GenerateBarcodeCharacters(string data)
+    {
+        yield return BarcodeCharacter.Start;
+
+        int sum = 19;
+        foreach (var bc in PadLast(data.SelectMany(ConvertToBarcodeCharacters).Take(20), 20, BarcodeCharacter.CC4))
         {
-            return GenerateBarcodeCharacters(data).SelectMany(t => ConvertToBars(t));
+            sum += (int)bc;
+            yield return bc;
         }
 
-        private static IEnumerable<BarcodeCharacter> GenerateBarcodeCharacters(string data)
+        yield return BarcodeCharacter.Num0 + 18 - (sum - 1) % 19;
+        yield return BarcodeCharacter.Stop;
+    }
+
+    private static IEnumerable<BarcodeCharacter> ConvertToBarcodeCharacters(char character)
+    {
+        if (character >= '0' && character <= '9')
         {
-            yield return BarcodeCharacter.Start;
+            yield return BarcodeCharacter.Num0 + character - '0';
+        }
+        else if (character == '-')
+        {
+            yield return BarcodeCharacter.Hyphen;
+        }
+        else if (character >= 'A' && character <= 'Z')
+        {
+            yield return BarcodeCharacter.CC1 + (character - 'A') / 10;
+            yield return BarcodeCharacter.Num0 + (character - 'A') % 10;
+        }
+    }
 
-            int sum = 19;
-            foreach (var bc in PadLast(data.SelectMany(ConvertToBarcodeCharacters).Take(20), 20, BarcodeCharacter.CC4))
-            {
-                sum += (int)bc;
-                yield return bc;
-            }
+    private static IEnumerable<Bar> ConvertToBars(BarcodeCharacter bc)
+    {
+        switch (bc)
+        {
+            case BarcodeCharacter.Num0:
+                yield return Bar.Long;
+                yield return Bar.Timing;
+                yield return Bar.Timing;
+                break;
+            case BarcodeCharacter.Num1:
+                yield return Bar.Long;
+                yield return Bar.Long;
+                yield return Bar.Timing;
+                break;
+            case BarcodeCharacter.Num2:
+                yield return Bar.Long;
+                yield return Bar.Down;
+                yield return Bar.Up;
+                break;
+            case BarcodeCharacter.Num3:
+                yield return Bar.Down;
+                yield return Bar.Long;
+                yield return Bar.Up;
+                break;
+            case BarcodeCharacter.Num4:
+                yield return Bar.Long;
+                yield return Bar.Up;
+                yield return Bar.Down;
+                break;
+            case BarcodeCharacter.Num5:
+                yield return Bar.Long;
+                yield return Bar.Timing;
+                yield return Bar.Long;
+                break;
+            case BarcodeCharacter.Num6:
+                yield return Bar.Down;
+                yield return Bar.Up;
+                yield return Bar.Long;
+                break;
+            case BarcodeCharacter.Num7:
+                yield return Bar.Up;
+                yield return Bar.Long;
+                yield return Bar.Down;
+                break;
+            case BarcodeCharacter.Num8:
+                yield return Bar.Up;
+                yield return Bar.Down;
+                yield return Bar.Long;
+                break;
+            case BarcodeCharacter.Num9:
+                yield return Bar.Timing;
+                yield return Bar.Long;
+                yield return Bar.Long;
+                break;
+            case BarcodeCharacter.Hyphen:
+                yield return Bar.Timing;
+                yield return Bar.Long;
+                yield return Bar.Timing;
+                break;
+            case BarcodeCharacter.CC1:
+                yield return Bar.Down;
+                yield return Bar.Up;
+                yield return Bar.Timing;
+                break;
+            case BarcodeCharacter.CC2:
+                yield return Bar.Down;
+                yield return Bar.Timing;
+                yield return Bar.Up;
+                break;
+            case BarcodeCharacter.CC3:
+                yield return Bar.Up;
+                yield return Bar.Down;
+                yield return Bar.Timing;
+                break;
+            case BarcodeCharacter.CC4:
+                yield return Bar.Timing;
+                yield return Bar.Down;
+                yield return Bar.Up;
+                break;
+            case BarcodeCharacter.CC5:
+                yield return Bar.Up;
+                yield return Bar.Timing;
+                yield return Bar.Down;
+                break;
+            case BarcodeCharacter.CC6:
+                yield return Bar.Timing;
+                yield return Bar.Up;
+                yield return Bar.Down;
+                break;
+            case BarcodeCharacter.CC7:
+                yield return Bar.Timing;
+                yield return Bar.Timing;
+                yield return Bar.Long;
+                break;
+            case BarcodeCharacter.CC8:
+                yield return Bar.Long;
+                yield return Bar.Long;
+                yield return Bar.Long;
+                break;
+            case BarcodeCharacter.Start:
+                yield return Bar.Long;
+                yield return Bar.Down;
+                break;
+            case BarcodeCharacter.Stop:
+                yield return Bar.Down;
+                yield return Bar.Long;
+                break;
+            default:
+                break;
+        }
+    }
 
-            yield return BarcodeCharacter.Num0 + 18 - (sum - 1) % 19;
-            yield return BarcodeCharacter.Stop;
+    private static IEnumerable<T> PadLast<T>(IEnumerable<T> src, int totalCount, T paddingValue)
+    {
+        int count = 0;
+        foreach (var item in src)
+        {
+            count++;
+            yield return item;
         }
 
-        private static IEnumerable<BarcodeCharacter> ConvertToBarcodeCharacters(char character)
+        for (int i = count; i < totalCount; i++)
         {
-            if (character >= '0' && character <= '9')
-            {
-                yield return BarcodeCharacter.Num0 + character - '0';
-            }
-            else if (character == '-')
-            {
-                yield return BarcodeCharacter.Hyphen;
-            }
-            else if (character >= 'A' && character <= 'Z')
-            {
-                yield return BarcodeCharacter.CC1 + (character - 'A') / 10;
-                yield return BarcodeCharacter.Num0 + (character - 'A') % 10;
-            }
+            yield return paddingValue;
         }
+    }
 
-        private static IEnumerable<Bar> ConvertToBars(BarcodeCharacter bc)
-        {
-            switch (bc)
-            {
-                case BarcodeCharacter.Num0:
-                    yield return Bar.Long;
-                    yield return Bar.Timing;
-                    yield return Bar.Timing;
-                    break;
-                case BarcodeCharacter.Num1:
-                    yield return Bar.Long;
-                    yield return Bar.Long;
-                    yield return Bar.Timing;
-                    break;
-                case BarcodeCharacter.Num2:
-                    yield return Bar.Long;
-                    yield return Bar.Down;
-                    yield return Bar.Up;
-                    break;
-                case BarcodeCharacter.Num3:
-                    yield return Bar.Down;
-                    yield return Bar.Long;
-                    yield return Bar.Up;
-                    break;
-                case BarcodeCharacter.Num4:
-                    yield return Bar.Long;
-                    yield return Bar.Up;
-                    yield return Bar.Down;
-                    break;
-                case BarcodeCharacter.Num5:
-                    yield return Bar.Long;
-                    yield return Bar.Timing;
-                    yield return Bar.Long;
-                    break;
-                case BarcodeCharacter.Num6:
-                    yield return Bar.Down;
-                    yield return Bar.Up;
-                    yield return Bar.Long;
-                    break;
-                case BarcodeCharacter.Num7:
-                    yield return Bar.Up;
-                    yield return Bar.Long;
-                    yield return Bar.Down;
-                    break;
-                case BarcodeCharacter.Num8:
-                    yield return Bar.Up;
-                    yield return Bar.Down;
-                    yield return Bar.Long;
-                    break;
-                case BarcodeCharacter.Num9:
-                    yield return Bar.Timing;
-                    yield return Bar.Long;
-                    yield return Bar.Long;
-                    break;
-                case BarcodeCharacter.Hyphen:
-                    yield return Bar.Timing;
-                    yield return Bar.Long;
-                    yield return Bar.Timing;
-                    break;
-                case BarcodeCharacter.CC1:
-                    yield return Bar.Down;
-                    yield return Bar.Up;
-                    yield return Bar.Timing;
-                    break;
-                case BarcodeCharacter.CC2:
-                    yield return Bar.Down;
-                    yield return Bar.Timing;
-                    yield return Bar.Up;
-                    break;
-                case BarcodeCharacter.CC3:
-                    yield return Bar.Up;
-                    yield return Bar.Down;
-                    yield return Bar.Timing;
-                    break;
-                case BarcodeCharacter.CC4:
-                    yield return Bar.Timing;
-                    yield return Bar.Down;
-                    yield return Bar.Up;
-                    break;
-                case BarcodeCharacter.CC5:
-                    yield return Bar.Up;
-                    yield return Bar.Timing;
-                    yield return Bar.Down;
-                    break;
-                case BarcodeCharacter.CC6:
-                    yield return Bar.Timing;
-                    yield return Bar.Up;
-                    yield return Bar.Down;
-                    break;
-                case BarcodeCharacter.CC7:
-                    yield return Bar.Timing;
-                    yield return Bar.Timing;
-                    yield return Bar.Long;
-                    break;
-                case BarcodeCharacter.CC8:
-                    yield return Bar.Long;
-                    yield return Bar.Long;
-                    yield return Bar.Long;
-                    break;
-                case BarcodeCharacter.Start:
-                    yield return Bar.Long;
-                    yield return Bar.Down;
-                    break;
-                case BarcodeCharacter.Stop:
-                    yield return Bar.Down;
-                    yield return Bar.Long;
-                    break;
-                default:
-                    break;
-            }
-        }
+    private enum BarcodeCharacter
+    {
+        Num0,
+        Num1,
+        Num2,
+        Num3,
+        Num4,
+        Num5,
+        Num6,
+        Num7,
+        Num8,
+        Num9,
+        Hyphen,
+        CC1,
+        CC2,
+        CC3,
+        CC4,
+        CC5,
+        CC6,
+        CC7,
+        CC8,
+        Start,
+        Stop,
+    }
 
-        private static IEnumerable<T> PadLast<T>(IEnumerable<T> src, int totalCount, T paddingValue)
-        {
-            int count = 0;
-            foreach (var item in src)
-            {
-                count++;
-                yield return item;
-            }
-
-            for (int i = count; i < totalCount; i++)
-            {
-                yield return paddingValue;
-            }
-        }
-
-        private enum BarcodeCharacter
-        {
-            Num0,
-            Num1,
-            Num2,
-            Num3,
-            Num4,
-            Num5,
-            Num6,
-            Num7,
-            Num8,
-            Num9,
-            Hyphen,
-            CC1,
-            CC2,
-            CC3,
-            CC4,
-            CC5,
-            CC6,
-            CC7,
-            CC8,
-            Start,
-            Stop,
-        }
-
-        public enum Bar
-        {
-            Long = 1,
-            Up = 2,
-            Down = 3,
-            Timing = 4,
-        }
+    public enum Bar
+    {
+        Long = 1,
+        Up = 2,
+        Down = 3,
+        Timing = 4,
     }
 }
--- CustomerBarcodeGenerator/Program.cs
+++ CustomerBarcodeGenerator/Program.cs
@@ -1,50 +1,34 @@
-﻿using System;
-using System.IO;
+﻿using CustomerBarcodeGenerator;
 
-namespace CustomerBarcodeGenerator
+if (args.Length < 2)
 {
-    class Program
+    Console.WriteLine("usage: <PostCode> <Number>");
+    Console.ReadLine();
+}
+else if (args[0].Length != 7)
+{
+    Console.WriteLine("PostCodeは、ハイフン無しで7文字指定してください");
+    Console.ReadLine();
+}
+else
+{
+    var cb = new CustomerBarcode();
+    var bars = cb.Generate(args[0] + args[1]);
+
+    using (var writer = new StreamWriter($"{args[0]}_{args[1]}.svg"))
     {
-        static void Main(string[] args)
+        writer.WriteLine(@"<?xml version=""1.0"" encoding=""utf-8""?>");
+        writer.WriteLine($@"<svg width=""79.8mm"" height=""3.6mm"" xmlns=""http://www.w3.org/2000/svg"" viewBox=""0 0 133 6"">");
+
+        writer.WriteLine("<g>");
+        int count = 0;
+        foreach (var bar in bars)
         {
-            if (args.Length < 2)
-            {
-                Console.WriteLine("usage: <PostCode> <Number>");
-                Console.ReadLine();
-            }
-            else if (args[0].Length != 7)
-            {
-                Console.WriteLine("PostCodeは、ハイフン無しで7文字指定してください");
-                Console.ReadLine();
-            }
-            else
-            {
-                Run(args[0], args[1]);
-            }
+            writer.WriteLine($@"<rect x=""{count * 2}"" y=""{((int)bar - 1) / 2 * 2}"" width=""1"" height=""{(3 - (int)bar / 2) * 2}"" />");
+            count++;
         }
+        writer.WriteLine("</g>");
 
-        private static void Run(string data1, string data2)
-        {
-            var cb = new CustomerBarcode();
-            var bars = cb.Generate(data1 + data2);
-
-            using (var writer = new StreamWriter($"{data1}_{data2}.svg"))
-            {
-                writer.WriteLine(@"<?xml version=""1.0"" encoding=""utf-8""?>");
-                writer.WriteLine($@"<svg width=""79.8mm"" height=""3.6mm"" xmlns=""http://www.w3.org/2000/svg"" viewBox=""0 0 133 6"">");
-
-                writer.WriteLine("<g>");
-                int count = 0;
-                foreach (var bar in bars)
-                {
-                    writer.WriteLine($@"<rect x=""{count * 2}"" y=""{((int)bar - 1) / 2 * 2}"" width=""1"" height=""{(3 - (int)bar / 2) * 2}"" />");
-                    count++;
-                }
-                writer.WriteLine("</g>");
-
-                writer.WriteLine("</svg>");
-            }
-        }
-
+        writer.WriteLine("</svg>");
     }
 }
